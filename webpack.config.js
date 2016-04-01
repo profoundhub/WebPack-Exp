abc27f4b9160b2131f7053439f9578b52dcf966a
Daniel Lim <github@profoundideation.com>
2016-04-01 19:59:08 -0400

    add webpack config file, commit before compile

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,12 @@
-Coming Soon!
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
